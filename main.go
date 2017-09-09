c6f2dd1ae74e3c4bd366519e0a8dfeb00911f4ed
Craig Hesling <craig@hesling.com>
2017-09-09 00:45:16 -0400

    Added the basic example service

---
+++ main.go
@@ -0,0 +1,164 @@
+// Craig Hesling
+// September 9, 2017
+//
+// This is an example OpenChirp service. It sets up arguments and the main
+// runtime event loop to process new device service links
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/openchirp/framework"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+const (
+	version string = "1.0"
+)
+
+const (
+	// Set this value to true to have the service publish a service status of
+	// "Running" each time it receives a device update event
+	//
+	// This could be used as a service alive pulse if enabled
+	// Otherwise, the service status will indicate "Started" at the time the
+	// service "Started" the client
+	runningStatus = true
+)
+
+func run(ctx *cli.Context) error {
+	/* Set logging level */
+	log.SetLevel(log.Level(uint32(ctx.Int("log-level"))))
+
+	log.Info("Starting Example Service ")
+
+	/* Start framework service client */
+	c, err := framework.StartServiceClientStatus(
+		ctx.String("framework-server"),
+		ctx.String("mqtt-server"),
+		ctx.String("service-id"),
+		ctx.String("service-token"),
+		"Unexpected disconnect!")
+	if err != nil {
+		log.Error("Failed to StartServiceClient: ", err)
+		return cli.NewExitError(nil, 1)
+	}
+	defer c.StopClient()
+	log.Info("Started service")
+
+	/* Post service status indicating I am starting */
+	err = c.SetStatus("Starting")
+	if err != nil {
+		log.Error("Failed to publish service status: ", err)
+		return cli.NewExitError(nil, 1)
+	}
+	log.Info("Published Service Status")
+
+	/* Start service main device updates stream */
+	log.Info("Starting Device Updates Stream")
+	updates, err := c.StartDeviceUpdates()
+	if err != nil {
+		log.Error("Failed to start device updates stream: ", err)
+		return cli.NewExitError(nil, 1)
+	}
+	defer c.StopDeviceUpdates()
+
+	/* Setup signal channel */
+	log.Info("Processing device updates")
+	signals := make(chan os.Signal)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+
+	/* Post service status indicating I started */
+	err = c.SetStatus("Started")
+	if err != nil {
+		log.Error("Failed to publish service status: ", err)
+		return cli.NewExitError(nil, 1)
+	}
+	log.Info("Published Service Status")
+
+	for {
+		select {
+		case update := <-updates:
+			/* If runningStatus is set, post a service status as an alive msg */
+			if runningStatus {
+				err = c.SetStatus("Running")
+				if err != nil {
+					log.Error("Failed to publish service status: ", err)
+					return cli.NewExitError(nil, 1)
+				}
+				log.Info("Published Service Status")
+			}
+
+			logitem := log.WithFields(log.Fields{"type": update.Type})
+
+			switch update.Type {
+			case framework.DeviceUpdateTypeRem:
+				logitem.Info("Removing device with id ", update.Id, " and config ", update.Config)
+			case framework.DeviceUpdateTypeUpd:
+				logitem.Info("Removing device for update with id", update.Id, " and config ", update.Config)
+				fallthrough
+			case framework.DeviceUpdateTypeAdd:
+				logitem.Info("Adding device")
+				c.SetDeviceStatus(update.Id, "Added device with id ", update.Id, " and config ", update.Config)
+				// devTopic := "openchirp/devices/" + update.Id + "/transducer"
+			}
+		case sig := <-signals:
+			log.WithField("signal", sig).Info("Received signal")
+			goto cleanup
+		}
+	}
+
+cleanup:
+
+	log.Warning("Shutting down")
+	err = c.SetStatus("Shutting down")
+	if err != nil {
+		log.Error("Failed to publish service status: ", err)
+	}
+	log.Info("Published service status")
+
+	return nil
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "example-service"
+	app.Usage = ""
+	app.Copyright = "See https://github.com/openchirp/example-service for copyright information"
+	app.Version = version
+	app.Action = run
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "framework-server",
+			Usage:  "OpenChirp framework server's URI",
+			Value:  "http://localhost:7000",
+			EnvVar: "FRAMEWORK_SERVER",
+		},
+		cli.StringFlag{
+			Name:   "mqtt-server",
+			Usage:  "MQTT server's URI (e.g. scheme://host:port where scheme is tcp or tls)",
+			Value:  "tls://localhost:1883",
+			EnvVar: "MQTT_SERVER",
+		},
+		cli.StringFlag{
+			Name:   "service-id",
+			Usage:  "OpenChirp service id",
+			EnvVar: "SERVICE_ID",
+		},
+		cli.StringFlag{
+			Name:   "service-token",
+			Usage:  "OpenChirp service token",
+			EnvVar: "SERVICE_TOKEN",
+		},
+		cli.IntFlag{
+			Name:   "log-level",
+			Value:  4,
+			Usage:  "debug=5, info=4, warning=3, error=2, fatal=1, panic=0",
+			EnvVar: "LOG_LEVEL",
+		},
+	}
+	app.Run(os.Args)
+}
